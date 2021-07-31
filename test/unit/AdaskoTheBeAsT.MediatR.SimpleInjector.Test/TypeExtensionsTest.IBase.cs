40ce48172c47b71dba9e6ca9e4b95f2fe856b92b
Adam Pluciński <adaskothebeast@gmail.com>
2021-07-31 14:30:44 +0200

    fix style

--- test/unit/AdaskoTheBeAsT.MediatR.SimpleInjector.Test/TypeExtensionsTest.IBase.cs
+++ test/unit/AdaskoTheBeAsT.MediatR.SimpleInjector.Test/TypeExtensionsTest.IBase.cs
@@ -2,8 +2,13 @@ namespace AdaskoTheBeAsT.MediatR.SimpleInjector.Test
 {
     public sealed partial class TypeExtensionsTest
     {
+#pragma warning disable S2326 // Unused type parameters should be removed
+        // ReSharper disable once UnusedTypeParameter
         private interface IBase<T>
         {
         }
+
+        // ReSharper disable once UnusedTypeParameter
+#pragma warning restore S2326 // Unused type parameters should be removed
     }
 }
