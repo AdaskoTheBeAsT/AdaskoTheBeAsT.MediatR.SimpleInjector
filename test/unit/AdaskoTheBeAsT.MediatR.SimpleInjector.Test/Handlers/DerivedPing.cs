ea6f8d497ea60b280ab6b16efa0cdde23be333e4
Adam Pluciński <adaskothebeast@gmail.com>
2024-01-28 13:52:34 +0100

    fix code smell

--- test/unit/AdaskoTheBeAsT.MediatR.SimpleInjector.Test/Handlers/DerivedPing.cs
+++ test/unit/AdaskoTheBeAsT.MediatR.SimpleInjector.Test/Handlers/DerivedPing.cs
@@ -1,7 +1,5 @@
 namespace AdaskoTheBeAsT.MediatR.SimpleInjector.Test.Handlers;
 
 #pragma warning disable S2094 // Classes should not be empty
-public sealed class DerivedPing : Ping
+public sealed class DerivedPing : Ping;
 #pragma warning restore S2094 // Classes should not be empty
-{
-}
